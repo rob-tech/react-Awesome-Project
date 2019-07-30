68ef45e23cf5db894976ddf3ecc10c4797ac56c9
rob <robertahuber88@gmail.com>
2019-07-30 12:30:37 +0200

    commit

---
+++ src/components/ProjAwesome.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import AwesomeNav from "./AwesomeNav";
+
+class ProjAwesome extends Component {
+    render  (){
+        return(
+        <>
+        <AwesomeNav/>  
+      
+       
+        </>
+        )
+      }
+    }
+
+export default ProjAwesome;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import ProjAwesome  from'./components/ProjAwesome';
+import AwesomeNav from'./components/AwesomeNav';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ProjAwesome />, document.getElementById('root'));
+ReactDOM.render(<AwesomeNav />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
